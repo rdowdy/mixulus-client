754f356873fb217f8ad6f57e9ed6e6b0ed5a1dbe
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-27 12:05:19 -0700

    Scaffold app files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    wiredep = require('wiredep').stream,
+    inject = require('gulp-inject')
+
+var jsSources = ['app/features/**/*.js'],
+    cssSources = ['app/css/**/*.css'],
+    htmlSources = ['./**/*.html'];
+
+var paths = ['./app/css/**/*.css', './app/features/app.module.js', './app/features/**/*.js'];
+
+gulp.task('inject', function() {
+    var sources = gulp.src(paths, { read: false });
+    return gulp.src('./app/index.html')
+        .pipe(wiredep())
+        .pipe(inject(sources, { relative: true }))
+        .pipe(gulp.dest('./app'));
+});
