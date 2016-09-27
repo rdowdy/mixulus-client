a99e24bf67c43bc62467eab5ed7186e4f51c36a1
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-27 15:53:01 -0700

    Simple login page. Inject dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var paths = ['./app/css/**/*.css', './app/features/app.module.js', './app/featur
 
 gulp.task('inject', function() {
     var sources = gulp.src(paths, { read: false });
-    return gulp.src('./app/index.html')
+    return gulp.src('./app/*.html')
         .pipe(wiredep())
         .pipe(inject(sources, { relative: true }))
         .pipe(gulp.dest('./app'));
