fd72df92ad8dc1d055fe25e83bcbd7b69d18c118
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-10 16:02:54 -0700

    Login, and then get a JWT for API endpoints.

--- app/features/auth/auth.controller.js
+++ app/features/auth/auth.controller.js
@@ -16,19 +16,28 @@
         ////////////////
 
         function login() {
-        	var user = {
-        		"username": vm.loginUsername,
-        		"password": vm.loginPassword
-        	};
+            var user = {
+                "username": vm.loginUsername,
+                "password": vm.loginPassword
+            };
 
-        	$http.post("/login", user).then(function(res) {
-        		if(res.data.success == true) {
-                    localStorageService.set('userId', res.data.user._id);
-        			$window.location.href = "/home";
-        		} else {
-        			// some sort of error
-        		}
-        	});
+            $http.post("/login", user).then(function(res) {
+                if (res.data.success == true) {
+                    // now get the JWT token for API endpoint
+                    $http.post('/authenticate', user).then(function(jwtRes) {
+                        if (jwtRes.data.success == true) {
+                            localStorageService.set('token', jwtRes.data.token);
+                            localStorageService.set('userId', res.data.user._id);
+                            $window.location.href = "/home";
+                        } else {
+                            // some sort of JWT error
+                        }
+                    })
+
+                } else {
+                    // some sort of login error
+                }
+            });
 
             //$http.post("/login", user);
         }
