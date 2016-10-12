5847d57ae2579ebf67ff35f29680ac2da8846642
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-12 14:45:55 -0700

    Display list of collaborators on dialog. Change button to 'Collaborators'. Style tweaks

--- app/features/dialog/dialog.controller.js
+++ app/features/dialog/dialog.controller.js
@@ -5,10 +5,10 @@
         .module('app')
         .controller('DialogController', DialogController);
 
-    DialogController.$inject = ['$http', '$scope', 'CollabFactory', 'localStorageService'];
+    DialogController.$inject = ['$http','CollabFactory', 'localStorageService'];
 
     /* @ngInject */
-    function DialogController($http, $scope, CollabFactory, localStorageService) {
+    function DialogController($http, CollabFactory, localStorageService) {
         var vm = this;
         vm.title = 'DialogController';
 
@@ -16,12 +16,21 @@
 
         ////////////////
 
+        getUsers();
+
+        function getUsers() {
+            vm.collabId = localStorageService.get('collabId');
+            CollabFactory.getCollabById(vm.collabId).then(function(res) {
+                vm.users = res.data;
+            })
+        }
+
         function addUserToCollab(username) {
         	$http.get("/users/search/" + username).then(function(res) {
         		if(res.data._id != null) {
-        			var collabId = localStorageService.get('collabId');
-        			CollabFactory.addUserToCollab(collabId, res.data._id).then(function(res) {
-        				$scope.closeThisDialog();
+        			CollabFactory.addUserToCollab(vm.collabId, res.data._id).then(function(res) {
+                        vm.users.userIds.push({username: username});
+                        vm.addUsername = "";
         			});
         		}
         	});
