2fa2d691d723f9b264de2055db1436f6502bbb0d
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-25 09:12:05 -0700

    Clean up AuthController

--- app/features/auth/auth.controller.js
+++ app/features/auth/auth.controller.js
@@ -10,22 +10,34 @@
     /* @ngInject */
     function AuthController($http, $window, localStorageService) {
         var vm = this;
+
+        /////////////////////
+        // Functions
+        /////////////////////
         vm.login = login;
         vm.signup = signup;
 
-        ////////////////
-
+        /////////////////////
+        // Function Definitions
+        /////////////////////
+        ///////////
+        // LOGIN
+        ///////////
         function login() {
             var user = {
                 "username": vm.loginUsername,
                 "password": vm.loginPassword
             };
 
+            // POST username/password to /login
+            // then POST again to /authenticate to get JWT token
             $http.post("/login", user).then(function(res) {
                 if (res.data.success == true) {
                     // now get the JWT token for API endpoint
                     $http.post('/authenticate', user).then(function(jwtRes) {
                         if (jwtRes.data.success == true) {
+                            // localStorageService will allow me to access token and userId
+                            // from other pages in the app
                             localStorageService.set('token', jwtRes.data.token);
                             localStorageService.set('userId', res.data.user._id);
                             $window.location.href = "/home";
@@ -42,6 +54,9 @@
             });
         }
 
+        ///////////
+        // SIGNUP
+        ///////////
         function signup() {
             var newUser = {
                 "username": vm.signupUsername,
@@ -49,13 +64,17 @@
             }
 
             if(vm.signupPassword != vm.signupPasswordConfirm) {
-                alert("Your passwords don't match!");
+                vm.signupError = "Your passwords don't match!";
             } else {
 
+                // POST username/password to /signup
+                // then POST again to /authenticate to get JWT
                 $http.post("/signup", newUser).then(function(res) {
                     if(res.data.success == true) {
                         $http.post("/authenticate", newUser).then(function(jwtRes) {
                             if(jwtRes.data.success == true) {
+                                // localStorageService will allow me to access token and userId
+                                // from other pages in the app
                                 localStorageService.set('token', jwtRes.data.token);
                                 localStorageService.set('userId', res.data.user._id);
                                 $window.location.href = "/home";
