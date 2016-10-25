3432d1b27c26dd708eca994956aebe9173fcfa4a
Ryan Dowdy <rdowdy21@gmail.com>
2016-10-25 14:17:59 -0700

    Clean up SoundFactory

--- app/features/core/sound.factory.js
+++ app/features/core/sound.factory.js
@@ -11,28 +11,47 @@
     function SoundFactory($http) {
         var service = {
             addSound: addSound,
+            deleteSound: deleteSound,
             getSoundById: getSoundById,
-            updateSound: updateSound,
-            deleteSound: deleteSound
+            updateSound: updateSound
         };
         return service;
 
         ////////////////
 
+        // POST /sounds
         function addSound(sound) {
         	return $http.post("/sounds", sound);
         }
 
+        // DELETE /sounds/:soundId
+        function deleteSound(sound) {
+            return $http.delete("/sounds/" + sound._id);
+        }
+
+        // GET /sounds/:soundId
+        // note: this may take a while since it will also send the buffer
         function getSoundById(soundId) {
             return $http.get("/sounds/" + soundId);
         }
 
+        // PUT /sounds/:soundId
         function updateSound(sound) {
+            sound = stripSound(sound);
             return $http.put("/sounds/" + sound._id, sound);
         }
 
-        function deleteSound(sound) {
-            return $http.delete("/sounds/" + sound._id);
+        // prepare the sound object for the database
+        function stripSound(sound) {
+            return {
+                _id: sound._id,
+                filePath: sound.filePath,
+                fps: sound.fps,
+                frameLength: sound.frameLength,
+                gridLocation: sound.gridLocation,
+                track: sound.track,
+                trackId: sound.trackId
+            }
         }
     }
 })();
